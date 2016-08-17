c0fa9ea414b17006f3e09016eb46161b8031b0a9
caio.hamamura <caiohamamura@gmail.com>
2016-08-16 22:38:55 -0300

    a

--- query.php
+++ query.php
@@ -5,6 +5,8 @@ if (isset($_GET['query'])) {
 		print "Não foi possível estabelecer uma conexão com o banco de dados.";
 	} else {
 		$results = pg_query($_GET['query']) or die('Query failed: ' . pg_last_error());
+		$fields = pg_field_name($results);
+		print json_encode($fields);
 		$myarray = array();
 		while ($row = pg_fetch_row($results)) {
   			$myarray[] = $row;
