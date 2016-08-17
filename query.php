53c49dac007dc47e448187d7e4dfae2fcd98de06
caio.hamamura <caiohamamura@gmail.com>
2016-08-16 22:33:30 -0300

    a

--- query.php
+++ query.php
@@ -1,9 +1,15 @@
 <?php 
+
 if (isset($_GET['query'])) {
 	if(!@($conexao=pg_connect ("host=apoema.esalq.usp.br dbname=modelo port=5432 user=getlidar password=***REMOVED***"))) {
 		print "Não foi possível estabelecer uma conexão com o banco de dados.";
 	} else {
-   		print $_GET['query'];
+		$results = pg_query($_GET['query']) or die('Query failed: ' . pg_last_error());
+		$myarray = array();
+		while ($row = pg_fetch_row($results)) {
+  			$myarray[] = $row;
+		}
+   		print json_encode($myarray);
 	}
 } else {
     print 'Query nao tah definida';
