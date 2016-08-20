5a1ac7977e041c8ac69540f6960c47dcf1a60cf4
caio.hamamura <caiohamamura@gmail.com>
2016-08-20 17:51:51 -0300

    corrigido

--- app/index.js
+++ app/index.js
@@ -13,41 +13,61 @@ class Main extends React.Component {
 			header: [],
 			transicao: false,
 			opacity: 0,
-			erro:''
+			erroOpacity: 0
 	    };
   	}
 
   	@autobind
 	executar() {
-		if (this.state.opacity === 1)
+		if (this.state.opacity === 1 || this.state.erroOpacity === 1)
 			this.setState({
 				transicao: true,
 				opacity: 0,
+				erroOpacity: 0,
 			});
-		axios.get('http://apoema.esalq.usp.br/~getlidar/query.php?query=' + this.state.query.replace(/\n/g, ' \n'))
+		axios.get('http://apoema.esalq.usp.br/~getlidar/query.php', {params: {query: this.state.query}})
 		  .then(function(response){
-		  	if(typeof(response.data) === 'string') {
-		  		this.setState({
-		  			erro: response.data,
-		  			header: [],
-		  			dados: [[]]
-		  		});
+		  	if (typeof(response.data) === 'string') {
+		  		console.log('string');
+		  		var fn = function() {
+			  		this.setState({
+			  			erro: response.data,
+			  			dados: [[]],
+						header: [],
+			  			erroOpacity: 1,
+			  		});
+			  		this.refs.tabela.removeEventListener('transitionend', fn, false);
+		  			this.refs.erro.removeEventListener('transitionend', fn, false);
+		  		}.bind(this);
+		  		if (this.state.transicao) {
+					this.refs.tabela.addEventListener('transitionend', fn, false); 	
+			  		this.refs.erro.addEventListener('transitionend', fn, false);
+		  		} else {
+					fn();
+		  		}
 		  	}
 		  	else {
 		  		var fn = function(){
+		  			console.log('rodou fn');
 		  			this.setState({
 					dados: response.data.dados,
 					header: response.data.campos,
 					opacity: 1,
-					erro: '',
 				});
-		  			this.refs.table.removeEventListener('transitionend', fn);
+		  			this.refs.tabela.removeEventListener('transitionend', fn, false);
+		  			this.refs.erro.removeEventListener('transitionend', fn, false);
+		  			console.log(JSON.stringify(this.state));
 		  		}.bind(this);
-			  	if (this.state.transicao)
-			  		this.refs.tabela.addEventListener('transitionend', fn); 	
-			  	else
+			  	if (this.state.transicao) {
+			  		console.log('em transicao');
+			  		this.refs.tabela.addEventListener('transitionend', fn, false); 	
+			  		this.refs.erro.addEventListener('transitionend', fn, false);
+			  	}
+			  	else {
+			  		console.log('já acabou transicao');
 				    fn();
-			  } 
+			  	}
+			  }
 		}.bind(this))
 	}
 
@@ -56,6 +76,24 @@ class Main extends React.Component {
 		this.setState({
 			query: e.target.value,
 		})
+	}
+
+	@autobind
+	transitionStopped() {
+		console.log('acabou transicao');
+		this.setState({
+					transicao: false,
+				});
+	}
+
+	componentDidMount() {
+		this.refs.tabela.addEventListener('transitionend', this.transitionStopped);
+		this.refs.erro.addEventListener('transitionend', this.transitionStopped);
+	}
+
+	componentWillUnmount() {
+	 	this.refs.tabela.removeEventListener('transitionend', this.transitionStopped);
+	 	this.refs.erro.removeEventListener('transitionend', this.transitionStopped);
 	}
 
 	render() {
@@ -84,9 +122,9 @@ class Main extends React.Component {
 						<div className="tabela-div" style={{opacity: this.state.opacity}} ref="tabela">
 							<Tabela header={this.state.header} data={this.state.dados} />
 						</div>
-						<div className="erro">
+					</div>
+					<div className="col-sm-12 erro" style={{opacity: this.state.erroOpacity}} ref="erro">
 							{this.state.erro}
-						</div>
 					</div>
 				</div>
 			</div>
