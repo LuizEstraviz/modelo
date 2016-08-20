fccb9427b1c81f2d0be8aeb04b970c5da273521e
caio.hamamura <caiohamamura@gmail.com>
2016-08-20 15:35:08 -0300

    corrigido

--- query.php
+++ query.php
@@ -6,7 +6,7 @@ if (isset($_GET['query'])) {
 	if(!@($conexao=pg_connect ("host=apoema.esalq.usp.br dbname=modelo port=5432 user=getlidar password=2T!SYAk%F%Us"))) {
 		print "Não foi possível estabelecer uma conexão com o banco de dados.";
 	} else {
-		$results = pg_query(str_replace("\n", PHP_EOL, $_GET['query']) or die(pg_last_error());
+		$results = pg_query(str_replace("\n", PHP_EOL, $_GET['query'])) or die(pg_last_error());
 		$fields = array();
 		for ($j = 0; $j < pg_num_fields($results); $j++) {
 			$fields[] = pg_field_name($results, $j);
