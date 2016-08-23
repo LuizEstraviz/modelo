77a6d7cbc98b1e12d32297f0b17f23d261b3f130
caio.hamamura <caiohamamura@gmail.com>
2016-08-23 16:15:45 -0300

    adiciona esquema e botao csv

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,14 @@ module.exports = {
   	loaders: [
   		{
         test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel']
-      }
+      },
+      {
+        test: /.*\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+        ]
+      },
   	]
   },
   plugins: [
