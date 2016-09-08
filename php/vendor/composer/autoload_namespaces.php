468a25874d8650eb55f10a524196136a343f8aba
caio.hamamura <caiohamamura@gmail.com>
2016-09-08 14:00:29 -0300

    Arruma psr4

--- php/vendor/composer/autoload_namespaces.php
+++ php/vendor/composer/autoload_namespaces.php
@@ -6,7 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Test' => array($baseDir . '/src'),
-    'Services' => array($baseDir . '/src'),
-    'Classes' => array($baseDir . '/src'),
 );
